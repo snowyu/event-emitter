5104989717b1fde2997879cf8b225cdcbfa5333a
Riceball LEE <snowyu.lee@gmail.com>
2015-02-11 15:44:35 +0800

    + test/test-event-emitter-num-args.js

--- test/events.js
+++ test/events.js
@@ -15,6 +15,7 @@ module.exports = function (events, t) {
   result['emitter-method-names'] =require('./test-event-emitter-method-names');
   result['listeners-side-effects'] =require('./test-event-emitter-listeners-side-effects');
   result['no-error-provided-to-error-event'] =require('./test-event-emitter-no-error-provided-to-error-event');
+  result['num-args'] =require('./test-event-emitter-num-args');
   return result;
 }
 
