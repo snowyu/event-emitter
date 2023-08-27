2f10e7a689e5fea4d36fe802bb9d96c036598066
Riceball LEE <snowyu.lee@gmail.com>
2023-08-27 09:35:18 +0800

    test: add async bubbling test

--- test/bubbling-test.js
+++ test/bubbling-test.js
@@ -2,6 +2,10 @@ import {assert} from "chai";
 
 import ee from '../src/wrap-event-emitter'
 
+async function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms))
+}
+
 describe('bubbling', () => {
 	it('should bubbling result', () => {
 		const ee1 = ee()
@@ -45,6 +49,57 @@ describe('bubbling', () => {
 		})
 
     assert.equal(ee1.emit('test', 1, 2), 3);
+    assert.equal(count, 2);
+	});
+
+  it('should bubbling result async', async () => {
+		const ee1 = ee()
+    let count = 0
+    ee1.result = 0
+		ee1.on('test', async function(a, b) {
+      await sleep(50)
+      const r = this.result || 0
+      this.result = a + b + r
+      ++count
+		})
+		ee1.on('test', async function(a, b) {
+      await sleep(50)
+      const r = this.result || 0
+      this.result = a + b + r
+      ++count
+		})
+		ee1.on('test', async function(a, b) {
+      await sleep(50)
+      ++count
+		})
+
+    assert.equal(await ee1.emitAsync('test', 1, 2), 6);
+    assert.equal(count, 3);
+	});
+
+  it('should stop bubbling async', async () => {
+		const ee1 = ee()
+    let count = 0
+    ee1.result = 0
+		ee1.on('test', async function(a, b) {
+      await sleep(50)
+      const r = this.result || 0
+      this.result = a + b + r
+      ++count
+		})
+		ee1.on('test', async function(a, b) {
+      await sleep(50)
+      this.stopped = true
+      ++count
+		})
+		ee1.on('test', async function(a, b) {
+      await sleep(50)
+      const r = this.result || 0
+      this.result = a + b + r
+      ++count
+		})
+
+    assert.equal(await ee1.emitAsync('test', 1, 2), 3);
     assert.equal(count, 2);
 	});
 });
